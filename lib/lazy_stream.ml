77ba9be9055b3591e6a349b47b1444f3db48f544
b0a04gl <balaji.foo404@gmail.com>
2023-11-26 14:01:24 +0530

    updated lazy_stream

--- lib/lazy_stream.ml
+++ lib/lazy_stream.ml
@@ -1,56 +1,51 @@
 (* lib/lazy_stream.ml *)
 
-  type 'a t = 'a lazy_stream_node Lazy.t
+type 'a t = 'a lazy_stream_node Lazy.t
 
-  and 'a lazy_stream_node =
-    | Cons of 'a * 'a t
-    | Nil
+and 'a lazy_stream_node =
+  | Cons of 'a * 'a t
+  | Nil
 
-  let empty = lazy Nil
+let empty = lazy Nil
 
-  let cons hd tl = lazy (Cons (hd, tl))
+let cons hd tl = lazy (Cons (hd, tl))
 
-   let rec force  =
-    fun stream ->
-      lazy (
-        match Lazy.force stream with
-        | Cons (hd, tl) ->
-          Cons (hd, force tl)
-        | Nil ->
-          Nil
-      )
-  
-  
-  
+let rec force stream =
+  lazy (
+    match Lazy.force stream with
+    | Cons (hd, tl) ->
+      Cons (hd, force tl)
+    | Nil ->
+      Nil
+  )
 
-  let rec take n stream =
-    if n <= 0 then []
-    else
-      match Lazy.force stream with
-      | Cons (hd, tl) -> 
-          hd :: take (n - 1) tl
-      | Nil -> []
+let rec take n stream =
+  if n <= 0 then []
+  else
+    match Lazy.force stream with
+    | Cons (hd, tl) ->
+      hd :: take (n - 1) tl
+    | Nil ->
+      []
 
+let rec map f stream =
+  lazy (
+    match Lazy.force stream with
+    | Cons (hd, tl) -> Cons (f hd, map f tl)
+    | Nil -> Nil
+  )
 
-      
-  let rec map f stream =
-    lazy (
-      match Lazy.force stream with
-      | Cons (hd, tl) -> Cons (f hd, map f tl)
-      | Nil -> Nil
-    )
+let rec filter pred stream =
+  lazy (
+    match Lazy.force stream with
+    | Cons (hd, tl) when pred hd -> Cons (hd, filter pred tl)
+    | Cons (_, tl) -> Lazy.force (filter pred tl)
+    | Nil -> Nil
+  )
 
-  let rec filter pred stream =
-    lazy (
-      match Lazy.force stream with
-      | Cons (hd, tl) when pred hd -> Cons (hd, filter pred tl)
-      | Cons (_, tl) -> Lazy.force (filter pred tl)
-      | Nil -> Nil
-    )
-
-  let rec from_list lst =
-    lazy (
-      match lst with
-      | [] -> Nil
-      | hd :: tl -> Cons (hd, from_list tl)
-    )
+let rec from_list lst =
+  lazy (
+    match lst with
+    | [] -> Nil
+    | hd :: tl -> Cons (hd, from_list tl)
+  )
